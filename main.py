e755ebe2def534794267508aefb0d3989027021b
Mila Hlinovsky <126323400+hlinovskym@users.noreply.github.com>
2023-10-27 20:42:03 +0200

    Update main.py
    description updated

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import pytz
 ################################################################
 ###       prepis cestu a pozor na milisekundy !!!            ###
 ################################################################
+###      milisekundy jsou odkomentovaný a řeší se jinak      ###
 
 PATH = "/.../"
 
